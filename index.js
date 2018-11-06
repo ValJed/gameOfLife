2bf851a590ba847a1aa5ce0e87c34e28d128ea90
val <valentin.jeudy@gmail.com>
2018-11-06 23:10:31 +0100

    getting for each cell alive neighbors

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ document.addEventListener("DOMContentLoaded", () => {
     return grid
   }
 
+  const createNudeGrid = (width, height) => {
+    const res = new Array(height)
+
+    for(var y = 0; y < height; y++) {
+      grid[y] = [];
+      for(var x = 0; x < width; x++) {
+        grid[y][x] = 0
+      }
+    }
+    return grid
+  }
+
   const drawCells = (ctx, grid) => {
     const height = grid.length
     const width = grid[0].length
@@ -28,18 +40,22 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   const lineExtractor = (line, x) => {
-    return line ? [line[x - 1], line[x], line[x + 1]] : []
+    return line ? [
+      ...line[x - 1] !== undefined ? [line[x - 1]] : [],
+      line[x],
+      ...line[x + 1] !== undefined ? [line[x + 1]] : []
+    ] : []
   }
 
   const alterGrid = (grid) => {
     const height = grid.length
     const width = grid[0].length
+    const newGrid = createNudeGrid(width, height)
+    // console.log('newGrid ===> ', newGrid)
     for(let y = 0; y < height; y++) {
       for(let x = 0; x < width; x++) {
         const currentLine = []
         const cell = grid[y][x]
-        // const cellLeft = grid[y][x - 1]
-        // const cellRight = grid[y][x + 1]
         if (grid[y][x - 1] !== undefined) {
           currentLine.push(grid[y][x - 1])
         }
@@ -47,13 +63,34 @@ document.addEventListener("DOMContentLoaded", () => {
         if (grid[y][x + 1] !== undefined) {
           currentLine.push(grid[y][x + 1])
         }
-        console.log('currentLine ===> ', currentLine)
 
-        const lineBefore = lineExtractor(grid[y - 1], x)
-        const lineAfter = lineExtractor(grid[y + 1], x)
+        const prevLine = lineExtractor(grid[y - 1], x)
+        const nextLine = lineExtractor(grid[y + 1], x)
 
+        // console.log('prevLine ===> ', prevLine)
+        // console.log('currentLine ===> ', currentLine)
+        // console.log('nextLine ===> ', nextLine)
+
+        const neighbors = prevLine.concat(currentLine, nextLine)
+        const aliveCells = neighbors.filter((n) => n === 1)
+        // console.log('aliveCells ===> ', aliveCells)
+
+        if (grid[y][x] === 0){
+          if ( aliveCells.length === 3) {
+            newGrid[y][x] = 1
+          } else {
+            newGrid[y][x] = 0
+          }
+        } else {
+          if (aliveCells.length !== 2 && aliveCells.length !== 3) {
+            newGrid[y][x] = 0
+          } else {
+            newGrid[y][x] = 1
+          }
+        }
       }
     }
+    // console.log('newGrid ===> ', newGrid)
   }
 
   const grid = createGrid(10, 10)
